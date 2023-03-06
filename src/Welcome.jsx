2404993054eff4ecb4fe433b864b58de801351df
Pieter <daenenp@delawareconsulting.com>
2023-03-06 11:10:13 +0100

    welcome screen edit

--- src/Welcome.jsx
+++ src/Welcome.jsx
@@ -1,7 +1,16 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 
 function Welcome() {
-  return <h1>Welcome!</h1>;
+  const [username, setUsername] = useState('');
+
+  useEffect(() => {
+    fetch('https://localhost:7127//api/test2')
+      .then(response => response.json())
+      .then(data => setUsername(data.username))
+      .catch(error => console.error(error));
+  }, []);
+
+  return <h1>Welcome {username}!</h1>;
 }
 
 export default Welcome;
